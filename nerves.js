024df81ecf0e887d9283145c6bcf263284091392
Max <mjann@ucsc.edu>
2019-01-06 08:59:08 -0800

    nav bar

---
+++ nerves.js
@@ -0,0 +1,16 @@
+function superhero(){
+    console.log("superheroes baby");
+    var target = document.getElementById("target");
+    if(target){
+        target.innerHTML = "<p> Here is a list of the best franchises in video games </p>"
+    }
+    console.log("processes completed");
+}
+
+function doubledribble(){
+    console.log("dribbling");
+    var target = document.getElementById("goodbye");
+    if(target){
+        target.innerHTML = "<h4>Base Gaming Series</h4><ul><li> Half Life </li><li> Metal Gear Solid </li><li> Gears of War </li><li> Halo </li><li> Resident Evil </li><li> Call of Duty</li><li> God of War</li><li> Grand Theft Auto </li><li> BioShock </li><li> Mario Party</li><li> Super Smath Brothers</li><li> Mortal Kombat</li><li> The Elder Scrolls </li><li> Mario Kart</li></ul>"
+    }
+}
